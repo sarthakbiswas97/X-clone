6f007cae86eee22ba2ffbe331cbaeac0dcbdad64
SarthakBiswas <sarthak.sbiswas@gmail.com>
2023-12-24 19:44:00 +0530

    Added Tweet Feed and Create Tweet

--- components/FeedCard/index.tsx
+++ components/FeedCard/index.tsx
@@ -5,33 +5,58 @@ import { FaRetweet } from "react-icons/fa6";
 import { IoIosStats } from "react-icons/io";
 import { IoBookmarkOutline } from "react-icons/io5";
 import { MdOutlineFileUpload } from "react-icons/md";
+import { Tweet } from "@/gql/graphql";
 
-const FeedCard: React.FC = () => {
+interface FeedCardProps {
+  data: Tweet;
+}
+
+const FeedCard: React.FC<FeedCardProps> = (props) => {
+  const { data } = props;
   return (
     <div className="border border-r-0 border-l-0 border-b-0 border-gray-600 p-5 transition-all cursor-pointer">
       <div className="grid grid-cols-12 gap-3">
         <div className="col-span-1">
-          <Image
-            src="https://avatars.githubusercontent.com/u/95311791?v=4"
-            alt="user avatar"
-            height={50}
-            width={50}
-          />
+          {data.author?.profileImageURL && (
+            <Image
+            className="rounded-full"
+              src={data.author?.profileImageURL}
+              alt="user avatar"
+              height={50}
+              width={50}
+            />
+          )}
         </div>
         <div className="col-span-11">
-          <h5>sarthak biswas</h5>
+          <h5>{data.author?.firstName} {data.author?.lastName}</h5>
           <p>
-            Client-side and Server-side Rendering with Client and Server
-            Components. Further optimized with Static and Dynamic Rendering on
-            the server with Next.js. Streaming on Edge and Node.js runtimes.
+            {data.content}
           </p>
           <div className="flex justify-between mt-4 font-light text-lg items-center text-gray-500 w-[90%]">
-              <div className=" hover:bg-slate-800 rounded-full hover:text-sky-500"> <FaRegComment /> </div>
-              <div className=" hover:bg-slate-800 rounded-full hover:text-green-500"> <FaRetweet /> </div>
-              <div className=" hover:bg-slate-800 rounded-full hover:text-pink-600"> <FaRegHeart /> </div>
-              <div className=" hover:bg-slate-800 rounded-full hover:text-sky-500"> <IoIosStats /> </div>
-              <div className=" hover:bg-slate-800 rounded-full hover:text-sky-500"> <IoBookmarkOutline /> </div>
-              <div className=" hover:bg-slate-800 rounded-full hover:text-sky-500"> <MdOutlineFileUpload /> </div>
+            <div className=" hover:bg-slate-800 rounded-full hover:text-sky-500">
+              {" "}
+              <FaRegComment />{" "}
+            </div>
+            <div className=" hover:bg-slate-800 rounded-full hover:text-green-500">
+              {" "}
+              <FaRetweet />{" "}
+            </div>
+            <div className=" hover:bg-slate-800 rounded-full hover:text-pink-600">
+              {" "}
+              <FaRegHeart />{" "}
+            </div>
+            <div className=" hover:bg-slate-800 rounded-full hover:text-sky-500">
+              {" "}
+              <IoIosStats />{" "}
+            </div>
+            <div className=" hover:bg-slate-800 rounded-full hover:text-sky-500">
+              {" "}
+              <IoBookmarkOutline />{" "}
+            </div>
+            <div className=" hover:bg-slate-800 rounded-full hover:text-sky-500">
+              {" "}
+              <MdOutlineFileUpload />{" "}
+            </div>
           </div>
         </div>
       </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import Image from "next/image";
 import { BsTwitterX } from "react-icons/bs";
-import { MdHomeFilled } from "react-icons/md";
+import { MdHomeFilled, MdOutlineGifBox, MdOutlineImage } from "react-icons/md";
 import { RiNotification4Line } from "react-icons/ri";
 import { FiSearch } from "react-icons/fi";
 import { IoBookmarkOutline } from "react-icons/io5";
@@ -15,6 +15,8 @@ import { graphqlClient } from "@/clients/api";
 import { verifyUserGoogleTokenQuery } from "@/graphql/query/user";
 import { useCurrentUser } from "@/hooks/user";
 import { useQueryClient } from "@tanstack/react-query";
+import { useCreateTweet, useGetAllTweets } from "@/hooks/tweet";
+import { Tweet } from "@/gql/graphql";
 
 interface twitterSidebarButtons {
   title: String;
@@ -58,7 +60,26 @@ const sidebarMenuItems: twitterSidebarButtons[] = [
 
 export default function Home() {
   const { user } = useCurrentUser();
+  const { tweets = [] } = useGetAllTweets();
+  const { mutate } = useCreateTweet();
+
   const queryClient = useQueryClient();
+
+  const [content, setContent] = useState("");
+
+  //file uploading logic
+  const handleSelectImage = useCallback(() => {
+    const input = document.createElement("input");
+    input.setAttribute("type", "file");
+    input.setAttribute("accept", "image/*");
+    input.click();
+  }, []);
+
+  const handleCreateTweet = useCallback(() => {
+    mutate({
+      content,
+    });
+  }, [content, mutate]);
 
   const handleLoginWithGoogle = useCallback(
     async (cred: CredentialResponse) => {
@@ -81,7 +102,7 @@ export default function Home() {
         console.error("GraphQL request error:", error);
         toast.error("Login Error");
       }
-      await queryClient.invalidateQueries(["curent-user"]);
+      await queryClient.invalidateQueries({ queryKey: ["current-user"] });
     },
     [queryClient]
   );
@@ -123,23 +144,55 @@ export default function Home() {
                 />
               )}
               <div>
-              <h3 className="text-lg">{user.firstName} {user.lastName}</h3>
+                <h3 className="text-lg">
+                  {user.firstName} {user.lastName}
+                </h3>
               </div>
             </div>
           )}
         </div>
         <div className="col-span-6 border-r-[0.1px] border-l-[0.1px] h-screen overflow-scroll overflow-x-hidden border border-gray-600">
-          <FeedCard />
-          <FeedCard />
-          <FeedCard />
-          <FeedCard />
-          <FeedCard />
-          <FeedCard />
-          <FeedCard />
-          <FeedCard />
-          <FeedCard />
-          <FeedCard />
-          <FeedCard />
+          <div>
+            <div className="border border-r-0 border-l-0 border-b-0 border-gray-600 p-5 transition-all cursor-pointer">
+              <div className="grid grid-cols-12 gap-3">
+                <div className="col-span-1">
+                  {user?.profileImageURL && (
+                    <Image
+                      className="rounded-full"
+                      src={user?.profileImageURL}
+                      alt="user avatar"
+                      height={50}
+                      width={50}
+                    />
+                  )}
+                </div>
+                <div className="col-span-11">
+                  <textarea
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
+                    className="w-full bg-transparent text-xl px-3 border-b border-slate-700"
+                    placeholder="What is happening?!"
+                    rows={3}
+                  ></textarea>
+                  <div className="mt-2 flex justify-between items-center">
+                    <div className="flex text-xl text-blue-500">
+                      <MdOutlineImage onClick={handleSelectImage} />
+                      <MdOutlineGifBox />
+                    </div>
+                    <button
+                      onClick={handleCreateTweet}
+                      className="bg-[#1d9bf0] rounded-full px-4 py-1 text-lg font-semibold"
+                    >
+                      Post
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {tweets?.map((tweet) =>
+            tweet ? <FeedCard key={tweet?.id} data={tweet as Tweet} /> : null
+          )}
         </div>
         <div className="col-span-3">
           {!user && (
